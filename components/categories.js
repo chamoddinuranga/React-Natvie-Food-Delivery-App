8769b64b88f95d70a7bb6cf037e44fb80e313fb1
chamoddinuranga <chamod3246@gmail.com>
2024-08-04 16:25:46 +0530

    commit3

--- components/categories.js
+++ components/categories.js
@@ -1,8 +1,10 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { categories } from "../constants";
+import { useState } from "react";
 
 export default function Catergories() {
+  const [activeCategory, setActiveCategory] = useState(null);
   return (
     <View className="mt-4">
       <ScrollView
@@ -12,11 +14,24 @@ export default function Catergories() {
         contentContainerStyle={{ paddingHorizontal: 20 }}
       >
         {categories.map((category, index) => {
+          let isActive = category.id === activeCategory;
+          let btnClass = isActive ? " bg-gray-600" : " bg-gray-200";
+          let textClass = isActive
+            ? "font-semibold text-gray-800"
+            : "text-gray-600";
           return (
-            <View
-              key={index}
-              className="flex justify-center items-center mr-6"
-            ></View>
+            <View key={index} className="flex justify-center items-center mr-6">
+              <TouchableOpacity
+                onPress={() => setActiveCategory(category.id)}
+                className={"p-1 rounded-full shadow bg-gray-200" + btnClass}
+              >
+                <Image
+                  style={{ width: 45, height: 45 }}
+                  source={category.image}
+                />
+              </TouchableOpacity>
+              <Text className={"text-sm"+textClass}>{category.name}</Text>
+            </View>
           );
         })}
       </ScrollView>
--- constants/index.js
+++ constants/index.js
@@ -2,42 +2,42 @@ export const categories = [
   {
     id: "1",
     name: "Fast Food",
-    image: require("../assets/images/fastfood.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "2",
     name: "Fruit Item",
-    image: require("../assets/images/fruit.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "3",
     name: "Rice Item",
-    image: require("../assets/images/rice.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "4",
     name: "Noodle Item",
-    image: require("../assets/images/noodle.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "5",
     name: "Vegetable Item",
-    image: require("../assets/images/vegetable.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "6",
     name: "Sea Food",
-    image: require("../assets/images/seafood.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "7",
     name: "Sweets",
-    image: require("../assets/images/sweets.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
   {
     id: "8",
     name: "Drink",
-    image: require("../assets/images/drink.png"),
+    image: require("../assets/images/categories/icons8-hamburger-96.png"),
   },
 ];
 
@@ -50,7 +50,7 @@ export const featured = [
       {
         id: `1`,
         name: "Hot Chicken",
-        image: require("../assets/images/hot-chicken.png"),
+        image: require("../assets/images/categories/icons8-cupcake-emoji-96.png"),
         description: "Hot Chicken is a spicy chicken dish",
         lng: 38.2145602,
         lat: -85.927079,
@@ -62,21 +62,21 @@ export const featured = [
           {
             id: "1",
             name: "Hot Chicken",
-            image: require("../assets/images/hot-chicken.png"),
+            image: require("../assets/images/dishes/download (1) (1).jpeg"),
             description: "Hot Chicken is a spicy chicken dish",
             price: 12.99,
           },
           {
             id: "2",
             name: "Hot Chicken",
-            image: require("../assets/images/hot-chicken.png"),
+            image: require("../assets/images/dishes/download (1) (1).jpeg"),
             description: "Hot Chicken is a spicy chicken dish",
             price: 12.99,
           },
           {
             id: "3",
             name: "Hot Chicken",
-            image: require("../assets/images/hot-chicken.png"),
+            image: require("../assets/images/dishes/download (1) (1).jpeg"),
             description: "Hot Chicken is a spicy chicken dish",
             price: 12.99,
           },
